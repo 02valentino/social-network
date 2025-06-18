1e8d182e3fe292be24e901b027574c457cacebd4
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-18 17:24:21 +0200

    network: add URL patterns for ProfileView in urls.py

--- network/urls.py
+++ network/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PostListView
+from .views import ProfileView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
+    path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
 ]
