9d926d757d4926e2321d6738d810b97ce15fdbc4
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-18 17:19:04 +0200

    network: create the Profile view

--- network/views.py
+++ network/views.py
@@ -1,8 +1,23 @@
 from django.views.generic import ListView
 from .models import Post
+from django.contrib.auth import get_user_model
+from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
+
+User = get_user_model()
 
 class PostListView(ListView):
     model = Post
     template_name = 'network/post_list.html'
     context_object_name = 'posts'
     ordering = ['-posted_at']
+
+class ProfileView(DetailView):
+    model = User
+    template_name = 'network/profile.html'
+    context_object_name = 'profile_user'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(User, username=self.kwargs['username'])
