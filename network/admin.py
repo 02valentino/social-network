acf77b082982129af6742cde504cf7fc3394e3ba
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-18 11:29:50 +0200

    network: register Post and Follow models in admin panel

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Follow
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content', 'timestamp')
+    search_fields = ('author__username', 'content')
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('follower', 'following', 'created')
+    search_fields = ('follower__username', 'following__username')
