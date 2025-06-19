8dfa1708d6dbb392afb3f1084df1f19e21798360
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 18:48:20 +0200

    users: ad EditProfileView class for user profile editing with UpdateView

--- users/views.py
+++ users/views.py
@@ -2,9 +2,25 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
+from .forms import ProfileUpdateForm
 
 class SignupView(CreateView):
     model = CustomUser
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+class EditProfileView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    form_class = ProfileUpdateForm
+    template_name = 'users/edit_profile.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy('profile', kwargs={'username': self.request.user.username})
