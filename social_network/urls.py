767a37f981d8039fda239ccc916fc5ba1be42e93
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 18:28:12 +0200

    Add static media file serving to urlpatterns using settings.MEDIA_URL and MEDIA_ROOT in urls.py

--- social_network/urls.py
+++ social_network/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('', include('network.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
