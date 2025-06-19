3dd25fc57ceb94ed391109d6539d196fabbca839
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 17:04:57 +0200

    network: add PostCreateView for authenticated users to create new posts

--- network/views.py
+++ network/views.py
@@ -3,6 +3,10 @@ from .models import Post
 from django.contrib.auth import get_user_model
 from django.views.generic import DetailView
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import PostForm
 
 User = get_user_model()
 
@@ -26,3 +30,13 @@ class ProfileView(DetailView):
         context = super().get_context_data(**kwargs)
         context['posts'] = self.object.posts.all().order_by('-posted_at')
         return context
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'network/create_post.html'
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
