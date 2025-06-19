16e2ecd6a754cc92bae3b78ea69fb43baeee7116
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 17:08:05 +0200

    network: update url.py to add PostCreateView route

--- network/urls.py
+++ network/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import PostListView
 from .views import ProfileView
+from .views import PostCreateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
