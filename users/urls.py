8c01c4ffdb9b1c2331cc7fe6116ca36ab8085fd5
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 18:52:16 +0200

    users: add edit URL pattern using EditProfileView

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SignupView
+from .views import EditProfileView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('edit/', EditProfileView.as_view(), name='edit-profile'),
 ]
