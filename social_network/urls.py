ab77bf3a204c1492eeb209e60c9adfb3652fdd73
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 16:26:13 +0200

    Update project URL configuration to include users app routes

--- social_network/urls.py
+++ social_network/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')),
     path('', include('network.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
