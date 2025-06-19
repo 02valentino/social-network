2a96aeb849714a5f1bc73f3d15ac5b896b84eb76
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 16:19:01 +0200

    users: add SignupView for user registration with CustomUserCreationForm

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class SignupView(CreateView):
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
