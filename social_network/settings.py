00913c11f4746c8c797a2e55aa25f4397579139c
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-19 18:26:00 +0200

    Configure MEDIA_URL and MEDIA_ROOT for user-uploaded media files in settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 LOGIN_REDIRECT_URL = '/' # Redirect URL after successful login
 LOGOUT_REDIRECT_URL = '/login/' # Redirect URL after logout
+
+# Media files (user uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
