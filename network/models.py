f2fd7725d5222556fd8070b05e0ab77fc69a23ef
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-21 01:58:28 +0200

    network: add liked_by ManyToManyField to Post model for tracking post likes

--- network/models.py
+++ network/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     posted_at = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return f"{self.author.username}'s post at {self.posted_at.strftime('%Y-%m-%d %H:%M')}"
