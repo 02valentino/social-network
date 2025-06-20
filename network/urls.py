d6411c7b4361dd19307459cc42c2860139b8cc20
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-20 15:52:14 +0200

    network: update urls.py to add user ban/unban and post deletion routes

--- network/urls.py
+++ network/urls.py
@@ -3,10 +3,13 @@ from .views import PostListView
 from .views import ProfileView
 from .views import PostCreateView
 from .views import ModeratorDashboardView
+from .views import ToggleBanUserView, DeleteAnyPostView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('moderator/', ModeratorDashboardView.as_view(), name='moderator-dashboard'),
+    path('moderator/toggle-ban/<int:user_id>/', ToggleBanUserView.as_view(), name='toggle-ban'),
+    path('moderator/delete-post/<int:post_id>/', DeleteAnyPostView.as_view(), name='delete-post'),
 ]
