fa8064a9283bd056421a9e2d77fa00a89641d9df
02valentino <valentino.hoxha@stud.unifi.it>
2025-06-21 02:42:00 +0200

    network: add CommentForm to handle user comment submissions in network/forms.py

--- network/forms.py
+++ network/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from .models import Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -7,4 +8,12 @@ class PostForm(forms.ModelForm):
         fields = ['content']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 4, 'placeholder': "What's on your mind?"}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 2, 'placeholder': 'Write a comment...'})
         }
