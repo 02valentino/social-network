49ba03771afb585e640a2b547a07ae1c3166ad4e
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-02 20:31:27 +0200

    users: add is_private field to CustomUser model for account privacy settings

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     is_banned = models.BooleanField(default=False)  # used by moderators
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
