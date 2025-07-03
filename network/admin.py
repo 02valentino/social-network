0208f2dc279ba39e43c8b09774bdb7cca304da9c
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-03 14:46:41 +0200

    network: remove FollowAdmin and add admin interface for FriendRequest in admin.py

--- network/admin.py
+++ network/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Follow
+from .models import Post, FriendRequest
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -7,7 +7,9 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('author__username', 'content')
     ordering = ('-posted_at',)
 
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
-    list_display = ('follower', 'following', 'created')
-    search_fields = ('follower__username', 'following__username')
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'accepted', 'timestamp')
+    list_filter = ('accepted',)
+    search_fields = ('sender__username', 'receiver__username')
+    ordering = ('-timestamp',)
