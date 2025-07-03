3cd20c3fb1a1622ff5b9ffe2d9db116a42c0fea9
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-03 14:02:48 +0200

    network: replace follow with friend request notification logic in signals.py

--- network/signals.py
+++ network/signals.py
@@ -1,15 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Follow, Notification
+from .models import FriendRequest, Notification
 
-@receiver(post_save, sender=Follow)
-def create_follow_notification(sender, instance, created, **kwargs):
+@receiver(post_save, sender=FriendRequest)
+def friend_request_notification(sender, instance, created, **kwargs):
     if created:
-        follower = instance.follower
-        following = instance.following
-        if follower != following:
-            Notification.objects.create(
-                recipient=following,
-                sender=follower,
-                message=f"{follower.username} started following you."
-            )
+        Notification.objects.create(
+            recipient=instance.receiver,
+            sender=instance.sender,
+            message=f"{instance.sender.username} sent you a friend request."
+        )
+    elif instance.accepted:
+        Notification.objects.create(
+            recipient=instance.sender,
+            sender=instance.receiver,
+            message=f"{instance.receiver.username} accepted your friend request."
+        )
