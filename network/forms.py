313708be33526a310697853226c284e12fe1adcb
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-04 18:41:48 +0200

    network: update PostForm to add post visibility form

--- network/forms.py
+++ network/forms.py
@@ -5,9 +5,10 @@ from .models import Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['content']
+        fields = ['content', 'visibility']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 4, 'placeholder': "What's on your mind?"}),
+            'visibility': forms.Select(attrs={'class': 'form-control'})
         }
 
 class CommentForm(forms.ModelForm):
