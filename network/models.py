fa9239d79ae16326a4b377e9878f489f8be3d8fc
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-04 16:25:21 +0200

    network: add like count and comment count methods to Post model

--- network/models.py
+++ network/models.py
@@ -10,6 +10,13 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.author.username}'s post at {self.posted_at.strftime('%Y-%m-%d %H:%M')}"
 
+    def total_likes(self):
+        return self.liked_by.count()
+
+    @property
+    def comment_count(self):
+        return self.comments.count()
+
 class FriendRequest(models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_requests', on_delete=models.CASCADE)
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_requests', on_delete=models.CASCADE)
