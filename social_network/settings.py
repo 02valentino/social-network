a87342a1c205307e988aaf3cb2c55a4430a946d0
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-05 17:48:32 +0200

    social_network: add BanCheckMiddleware to middleware configuration

--- social_network/settings.py
+++ social_network/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.BanCheckMiddleware',
 ]
 
 ROOT_URLCONF = 'social_network.urls'
