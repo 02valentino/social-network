b4e629777a4453915945354e83b54b260cefc362
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-05 17:44:18 +0200

    users: add URL pattern to custom login view in urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import SignupView
-from .views import EditProfileView
+from .views import SignupView, EditProfileView, CustomLoginView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('edit/', EditProfileView.as_view(), name='edit-profile'),
 ]
