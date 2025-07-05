8cc5cc82da5a34b9cfd6264b7cd7409c9107d724
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-05 18:01:06 +0200

    social_network: remove login URL pattern from root urlpatterns (now handled in app-level urls)

--- social_network/urls.py
+++ social_network/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
     path('', include('network.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
