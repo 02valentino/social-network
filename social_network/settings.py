4cf182a818d170ee9e2ecfaf838057080ee3a66c
02valentino <valentino.hoxha@stud.unifi.it>
2025-07-11 09:21:58 +0200

    Switch to PostgreSQL via dj_database_url

--- social_network/settings.py
+++ social_network/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -89,10 +90,7 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
